230f51792b5aadfba75c4b23e6846bead4c4d5bd
kato.yut <kato.yut@mazda.co.jp>
2023-01-11 13:22:46 +0900

    シェルコマンドを呼び出すテストを追加

--- step_impl/step_impl.py
+++ step_impl/step_impl.py
@@ -1,3 +1,5 @@
+import subprocess
+from subprocess import CompletedProcess
 from getgauge.python import step, before_scenario, Messages
 
 vowels = ["a", "e", "i", "o", "u"]
@@ -28,6 +30,35 @@ def assert_words_vowel_count(table):
     actual = [str(number_of_vowels(word)) for word in table.get_column_values_with_name("Word")]
     expected = [str(count) for count in table.get_column_values_with_name("Vowel Count")]
     assert expected == actual
+
+
+@step("<dir_path> ディレクトリを作る")
+def assert_mkdir(dir_path):
+    result: CompletedProcess = subprocess.run(["mkdir", dir_path], stdout=subprocess.DEVNULL)
+    assert 0 == result.returncode
+
+
+@step("<dir_path> ディレクトリへの移動は <expected> する")
+def assert_cd(dir_path, expected):
+    result: CompletedProcess = subprocess.run(
+        ["cd", dir_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+    if expected == "成功":
+        assert 0 == result.returncode
+    else:
+        assert 1 == result.returncode
+
+
+@step("ディレクトリの中身をリストする")
+def assert_ls():
+    result: CompletedProcess = subprocess.run(["ls", "-l"], stdout=subprocess.DEVNULL)
+    assert 0 == result.returncode
+
+
+@step("<dir_path> ディレクトリを削除する")
+def assert_rm(dir_path):
+    result: CompletedProcess = subprocess.run(["rm", "-rf", dir_path], stdout=subprocess.DEVNULL)
+    assert 0 == result.returncode
 
 
 # ---------------
